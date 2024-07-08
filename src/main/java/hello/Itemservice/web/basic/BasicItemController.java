26cd1d9ebc112cceebefc483b3997d19cff028cc
DESKTOP-231GTAA\USER <snf00020@naver.com>
2024-07-09 00:57:18 +0900

    product List, product detail, product add

---
+++ src/main/java/hello/Itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,57 @@
+package hello.Itemservice.web.basic;
+
+import hello.Itemservice.domain.item.Item;
+import hello.Itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemid}")
+    public String item(@PathVariable long itemid, Model model) {
+        Item item = itemRepository.findById(itemid);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
+    }
+
+    //테스트용 데이터 추가
+
+    @PostConstruct
+    public void init() {
+
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
